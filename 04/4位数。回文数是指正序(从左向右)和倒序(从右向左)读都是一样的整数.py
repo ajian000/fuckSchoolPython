73ded03b7d145703815d0997b234ff33f0564f2a
阿简 <122674349+ajian000@users.noreply.github.com>
2023-04-12 18:44:28 +0800

    Update 4位数。回文数是指正序(从左向右)和倒序(从右向左)读都是一样的整数.py

--- 04/4位数。回文数是指正序(从左向右)和倒序(从右向左)读都是一样的整数.py
+++ 04/4位数。回文数是指正序(从左向右)和倒序(从右向左)读都是一样的整数.py
@@ -5,3 +5,15 @@ if(boolean1 and boolean2):
     print(int(number),"是回文数")
 else:
     print(int(number),"不是回文数")
+    
+#以下为该问题的另一种解法实现
+number = int(input("请输入一个四位数"))
+#a，b，c，d分别代表该数的千，百，十，个位
+a = number//1000
+b = (number%1000)//100
+c = ((number%1000)%100)//10
+d = ((number%1000)%100)%10
+if(a==d and b==c):
+    print(number,"是回文数")
+else:
+    print(number,"不是回文数")
